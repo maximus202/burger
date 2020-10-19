b7e60cd6b847e4cf9299d1a43e105810b5a2a8a0
Max R <maxx.ratto@gmail.com>
2020-10-19 08:01:49 -0600

    Added Layout to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <p>test</p>
+        </Layout>
       </div>
     )
   }
