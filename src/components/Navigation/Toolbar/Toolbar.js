7690875b683952af3c8c140e682b912ac99d62a4
Max R <maxx.ratto@gmail.com>
2020-10-24 13:27:06 -0600

    Navigation Items now rendering

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 import './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={'Toolbar'}>
         <div>Menu</div>
         <Logo />
-        <ul>
-            ...
-        </ul>
+        <nav>
+            <NavigationItems />
+        </nav>
     </header>
 );
 
