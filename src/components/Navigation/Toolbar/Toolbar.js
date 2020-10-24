7b97f0fa38155f66f5f9d4b64756c70bbb1ea2e4
Max R <maxx.ratto@gmail.com>
2020-10-24 12:34:09 -0600

    Logo now rendering

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './Toolbar.css';
+import Logo from '../../Logo/Logo';
 
 const toolbar = (props) => (
     <header className={'Toolbar'}>
         <div>Menu</div>
-        <div>Logo</div>
+        <Logo />
         <ul>
             ...
         </ul>
