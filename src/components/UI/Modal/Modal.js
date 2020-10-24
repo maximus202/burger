425681193771f1945b785f6d46fd9736387c2aaa
Max R <maxx.ratto@gmail.com>
2020-10-23 19:14:08 -0600

    Modal now comes up when order now is clicked

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,7 +3,12 @@ import React from 'react';
 import './Modal.css';
 
 const modal = (props) => (
-    <div className={'Modal'}>
+    <div
+        className={'Modal'}
+        style={{
+            transform: props.show ? 'translateY(0)' : 'translateY(-100)',
+            opacity: props.show ? '1' : '0'
+        }}>
         {props.children}
     </div>
 );
